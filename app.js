67779b2529d5afc836e03fdc2caa27548133e132
Abdullah Aslan <abdullah.aslan.se@gmail.com>
2024-12-09 21:02:39 +0300

    .

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+
+app.get("/", (req, res) => {
+  res.status(200).send("hello");
+})
+
+app.get("/end", (req, res) => {
+    res.status(200).send("Thank you for listening!");
+})
+  
+app.listen(port, () => {
+  console.log(`AWS app listening on port ${port}`);
+})
